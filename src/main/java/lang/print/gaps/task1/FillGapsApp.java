0c5ad741744c9dd3cad19ba7ba99db56b903c0f7
cheslav <v.shlapatsky@gmail.com>
2022-06-29 20:53:27 +0300

    Two

--- src/main/java/lang/print/gaps/task1/FillGapsApp.java
+++ src/main/java/lang/print/gaps/task1/FillGapsApp.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task1;
 
 public class FillGapsApp {
     public static void main(String[] args) {
-    System.out.println("Fixed\n");
+    System.out.println("Fixed");
     }
 }
--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -3,10 +3,7 @@ package lang.print.gaps.task1;
 public class PrintSentenceApp {
 
         public static void main(String[] args) {
-            String str = "This is my first java program";
-            String[] words = str.split(" ");
-            for (String word : words) {
-                System.out.println(word);
+            System.out.println("This is my first java program");
             }
         }
     }
